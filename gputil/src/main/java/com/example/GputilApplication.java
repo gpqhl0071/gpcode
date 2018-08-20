557dd44a90fa459d8ec03043184eeab00b3944f6
高鹏 <1110@forex.com.cn>
2018-08-20 15:55:30 +0800

    增加AES加解密工具类

---
+++ gputil/src/main/java/com/example/GputilApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GputilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GputilApplication.class, args);
+	}
+}
