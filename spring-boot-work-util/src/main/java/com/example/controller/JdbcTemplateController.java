1268e5959fee73332d71fc69298e3d42aa6b3c88
高鹏 <1110@forex.com.cn>
2018-09-28 15:19:02 +0800

    增加controller 及 打包插件

---
+++ spring-boot-work-util/src/main/java/com/example/controller/JdbcTemplateController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import com.example.work.util.service.TableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author gao peng
+ * @date 2018/9/28 14:47
+ */
+@RestController()
+@RequestMapping(value = "/jdbcTemplate")
+public class JdbcTemplateController {
+  @Autowired
+  private TableService tableService;
+
+  @RequestMapping(value = "/generator/{tableName}", method = RequestMethod.GET)
+  public void generator(@PathVariable("tableName") String tableName, HttpServletResponse response) throws Exception {
+    String result = tableService.generatorJDBCTemplate(tableName);
+
+    System.out.println(result);
+
+    response.getOutputStream().write(result.getBytes());
+  }
+}
