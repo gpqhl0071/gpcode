ab975e7f18874edeea1a087f14db53892c8ff235
高鹏 <1110@forex.com.cn>
2018-12-20 18:03:51 +0800

    增加日期计算

---
+++ gputil/src/main/java/com/example/redUtil/DateU.java
@@ -0,0 +1,31 @@
+package com.example.redUtil;
+
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.date.DateUnit;
+import cn.hutool.core.date.DateUtil;
+
+import java.util.Date;
+
+/**
+ * @author gao peng
+ * @date 2018/12/20 17:15
+ */
+public class DateU {
+  public static void main(String[] args) {
+    String str1 = DateUtil.format(new Date(), "yyyy-MM-dd 20:00:00");
+    String str2 = DateUtil.format(new Date(), "yyyy-MM-dd 10:00:00");
+    DateTime d1 = DateUtil.parseDateTime(str1);
+    DateTime d2 = DateUtil.parseDateTime(str2);
+
+    Date d = new Date();
+    d.getTime();
+
+
+    System.out.println(((d1.getTime() - d.getTime()) / 1000) + "s");
+
+    DateTime dt = new DateTime();
+    System.out.println(dt.between(d1.toJdkDate(), DateUnit.SECOND));
+    System.out.println(dt.between(d2.toJdkDate(), DateUnit.SECOND));
+    System.out.println(d1.compareTo(d2));
+  }
+}
