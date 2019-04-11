bc16848649c5b9f3b01db0be149203e3ed6471c4
高鹏 <1110@forex.com.cn>
2019-04-11 11:38:46 +0800

    aps test api

--- spring-boot-swagger/src/main/java/com/example/controller/ApsLoginController.java
+++ spring-boot-swagger/src/main/java/com/example/controller/ApsLoginController.java
@@ -11,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.security.Key;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -22,12 +24,9 @@ import java.util.Random;
 @RequestMapping(value = "/apsLogin")
 public class ApsLoginController {
 
-  @RequestMapping(value = "/{phone}/{password}",
-    method = RequestMethod.GET,
-    produces = "application/json;charset=UTF-8")
-  public String apsLogin(@PathVariable("phone") String phone,
-                         @PathVariable("password") String password,
-                         HttpServletResponse response) throws Exception {
+  @RequestMapping(produces = "application/json;charset=UTF-8")
+  public String apsLogin(HttpServletResponse response,
+                         HttpServletRequest request) throws Exception {
 
     Map jsonMap = new HashMap();
 
@@ -36,15 +35,25 @@ public class ApsLoginController {
     jsonMap.put("clientOSVersion", "4.4.4");
     jsonMap.put("clientVersionCode", 16);
     jsonMap.put("clientModel", "HM NOTE 1LTE");
-    jsonMap.put("userName", phone);
     jsonMap.put("clientPlatform", 2);
-    jsonMap.put("password", password);
+//    jsonMap.put("userName", phone);
+//    jsonMap.put("password", password);
+
+    Enumeration enumeration = request.getParameterNames();
+    Map<String, Object> modelMap = new HashMap<>();
+    while (enumeration.hasMoreElements()) {
+      String parameterName = (String) enumeration.nextElement();
+      jsonMap.put(parameterName, request.getParameter(parameterName));
+    }
 
     Map map = new HashMap();
 
     map.put("dxjcontent", encrypt(JSONUtil.toJsonStr(jsonMap), "Zi1qJmtGJk02MHghZSRyNQ=="));
 
-    String result = HttpUtil.post("https://192.168.9.10:8081/user/login/" + RandomUtil.randomNumbers(10), map);
+    String url = request.getParameter("url");
+
+    String result = HttpUtil.post("https://testaps.1001.co" + url + RandomUtil.randomNumbers(10), map);
+//    String result = HttpUtil.post("https://testaps.1001.co/user/login/" + RandomUtil.randomNumbers(10), map);
 
     result = decrypt(result, "SCpqJmtEdSZNNjd4I2Ukcg==");
 
