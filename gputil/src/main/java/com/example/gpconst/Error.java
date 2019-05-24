fccaf125af13286c730f0a8b76c0af5ab2d90d95
gao peng <1110@forex.com.cn>
2019-05-24 15:56:24 +0800

    error enum

---
+++ gputil/src/main/java/com/example/gpconst/Error.java
@@ -0,0 +1,31 @@
+package com.example.gpconst;
+
+/**
+ * @author gao peng
+ * @date 2019/5/24 15:39
+ */
+public enum Error {
+  DATABASE(0, "A database error has occured."),
+  DUPLICATE_USER(1, "This user already exists.");
+
+  private final int code;
+  private final String description;
+
+  private Error(int code, String description) {
+    this.code = code;
+    this.description = description;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  @Override
+  public String toString() {
+    return code + ": " + description;
+  }
+}
