4a990ab678978885dbf19881c88c3e2ae2753992
gao peng <1110@forex.com.cn>
2019-06-28 15:11:23 +0800

    add invoke

--- gputil/src/main/java/com/example/demo/Demo.java
+++ gputil/src/main/java/com/example/demo/Demo.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
 
+import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,7 +16,9 @@ public class Demo {
 
   public static void main(String[] args) {
 
-    System.out.println(DateUtil.today());
+
+    System.out.println(DateUtil.format(DateUtil.offsetDay(new Date(), -1), "yyyy-MM-dd 00:00:00"));
+    System.out.println(DateUtil.format(new Date(), "yyyy-MM-dd 00:00:00"));
 
   }
 
