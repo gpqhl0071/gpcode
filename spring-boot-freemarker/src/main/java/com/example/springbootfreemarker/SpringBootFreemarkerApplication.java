c6b415834323889463d75e6b4271f80e55bff897
gao peng <1110@forex.com.cn>
2019-08-21 10:15:30 +0800

    add freemarker

---
+++ spring-boot-freemarker/src/main/java/com/example/springbootfreemarker/SpringBootFreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootfreemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemarkerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootFreemarkerApplication.class, args);
+  }
+
+}
